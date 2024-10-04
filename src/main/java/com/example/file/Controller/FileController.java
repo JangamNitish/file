4fc6e4eb1d7c97631171f53ff909c359cdc2adf1
JangamNitish <129840077+JangamNitish@users.noreply.github.com>
2024-10-04 15:15:37 +0530

    File

--- src/main/java/com/example/file/Controller/FileController.java
+++ src/main/java/com/example/file/Controller/FileController.java
@@ -6,9 +6,7 @@ import com.example.file.Service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
